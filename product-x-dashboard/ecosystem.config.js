8e652eb83f11dc6542d0c2b2d5644bf8ef2c1e0d
Navaneeth <knavaneethan1@gmail.com>
2025-11-19 19:56:34 +0530

    PM2 added

---
+++ product-x-dashboard/ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "product-x-dashboard",
+      script: "npm",
+      args: "run preview -- --host 0.0.0.0 --port 5000",
+      cwd: "/root/Product X/product-x-dashboard",
+      env: {
+        NODE_ENV: "production"
+      },
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z"
+    }
+  ]
+}
