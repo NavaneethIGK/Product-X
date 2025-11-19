0677c27363fdb10aadcc5f5adf652cc8a5c69122
Navaneeth <knavaneethan1@gmail.com>
2025-11-19 20:15:10 +0530

    Config updated

--- product-x-dashboard/ecosystem.config.js
+++ product-x-dashboard/ecosystem.config.js
@@ -1,15 +1,27 @@
 module.exports = {
   apps: [
     {
+      name: "copilot-backend",
+      script: "python3",
+      args: "copilot_backend.py",
+      cwd: "/home/ubuntu/Product-X",
+      env: {
+        PYTHONUNBUFFERED: 1
+      },
+      error_file: "./logs/backend-err.log",
+      out_file: "./logs/backend-out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z"
+    },
+    {
       name: "dashboard",
       script: "npm",
       args: "run dev",
-      cwd: "/home/ubuntu/Product\\ X/product-x-dashboard",
+      cwd: "/home/ubuntu/Product-X/product-x-dashboard",
       env: {
         NODE_ENV: "development"
       },
-      error_file: "./logs/err.log",
-      out_file: "./logs/out.log",
+      error_file: "./logs/dashboard-err.log",
+      out_file: "./logs/dashboard-out.log",
       log_date_format: "YYYY-MM-DD HH:mm:ss Z"
     }
   ]
